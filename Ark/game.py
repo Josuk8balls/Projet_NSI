09e3e212c3760f65f2e0a1a1ca5153621e0551a3
Leo GAO <90462079+Josuk8balls@users.noreply.github.com>
2022-01-09 16:01:31 +0100

    Delete Ark directory

--- Ark/game.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import sys
-
-pygame.init() 
-
-screen = pygame.display.set_mode((800,450))
-pygame.display.set_caption("land") 
-fond = pygame.image.load('Image_Map\map1.png')
-Map = class.Map()
